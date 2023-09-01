189eb08c1c8b117f2710e50737b2d2e9d95bde26
AdamSlayer <adam.slajer@gmail.com>
2023-09-01 11:33:09 +0200

    Added basic texture loading and rendering. Added rotation to physics. Other minor changes

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ extern crate sdl2;
 
 use rand::Rng;
 use sdl2::event::Event;
+use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
-use sdl2::render::Canvas;
+use sdl2::render::{Canvas, Texture};
 use sdl2::video::Window;
 use std::collections::HashMap;
 use std::time::{Duration, Instant};
+
 
 
 /// Contains position, velocity, weight, and has some useful methods
@@ -20,19 +22,27 @@ struct Physics {
     y: f64,
     xvel: f64,
     yvel: f64,
-    weight: f64
+    weight: f64,
+    /// degrees
+    rot: f64,
+    /// degrees per second
+    rotvel: f64,
 }
 impl Physics {
     /// Applies a one time force in a specified direction, suddenly changing velocity. Has lower impact on heavier objects.
-    fn push() {
-        todo!()
+    fn push(&mut self, f: (f64, f64)) {
+        self.xvel += f.0/self.weight;
+        self.yvel += f.1/self.weight;
     }
     
     fn update(&mut self, delta: u128) {
         self.x += self.xvel * (delta as f64 / 1_000_000.);
         self.y += self.yvel * (delta as f64 / 1_000_000.);
+        self.rot += self.rotvel * (delta as f64 / 1_000_000.);
+
         self.xvel *= (-(delta as f64)/1_000_000.).exp();
         self.yvel *= (-(delta as f64)/1_000_000.).exp();
+        self.rotvel *= (-(delta as f64)/1_000_000.).exp();
     }
 }
 
@@ -45,18 +55,34 @@ struct Shape {
 /// A tank. Player, bot, boss etc
 struct Tank {
     physics: Physics,
-    hp: u32
-
+    hp: u32,
+    /// How much power the tank can allpy to it's movement. Will move faster with more power, but slower is it weights more.
+    power: f64,
 }
 impl Tank {
-    fn render(&self, canvas: &mut Canvas<Window>, camera: &Camera) {
-        canvas.set_draw_color(Color::RGB(255, 255, 255));
-        // prototype, only draws one point instead of full tank
-        canvas.draw_rect(Rect::from_center((
-            (self.physics.x - camera.x) as i32,
-            (self.physics.y - camera.y) as i32),
-            10, 10
-        )).unwrap();
+    fn render(&self, canvas: &mut Canvas<Window>, camera: &Camera, textures: &HashMap<String, Texture>) {
+        let texture = textures.get("tank").unwrap();
+        canvas.copy_ex(
+            &texture, None,
+            Rect::from_center(
+                Point::from((self.physics.x as i32, self.physics.y as i32)), // set center position
+                100, 100  // set render width and height
+            ),
+            self.physics.rot, // set rotation
+            Point::from((50,50)), // set center of rotation, in screen coordinates (not texture coordinates)
+            false, false).unwrap();
+    }
+
+
+    fn move_in_dir(&mut self, dir: (f64, f64)) {
+        // noramlize vector
+        let magnitude = (dir.0 * dir.0 + dir.1 * dir.1).sqrt();
+        if magnitude != 0.0 {
+            let normalized_dir = (dir.0 / magnitude, dir.1 / magnitude);
+            self.physics.push((normalized_dir.0*self.power, normalized_dir.1*self.power));
+        } else {
+            println!("attempt to move a tank in the direction (0.,0.)");
+        }
     }
 }
 
@@ -224,6 +250,12 @@ fn main() {
     // Initialize other libraries. So far only the rand crate
     let mut rng = rand::thread_rng();
 
+    // load textures. will be moved to its own function in the future
+    let texture_creator = canvas.texture_creator();
+    // Hashmap of all the textures used in the game. Later will read all textures form the textures folder and add them to the hashmap by the filename without the extension
+    let mut textures: HashMap<String, Texture> = HashMap::new();
+    textures.insert("tank".to_owned(), texture_creator.load_texture("textures/t.png").unwrap());
+
     // Initialize my own things
     let mut map = Map::new();
     let mut input = Input::init();
@@ -245,8 +277,11 @@ fn main() {
                 xvel: 0.,
                 yvel: 0.,
                 weight: 100.,
+                rot: 0.,
+                rotvel: 9000.
             },
-            hp: 100
+            hp: 100,
+            power: 100.
         }
     );
 
@@ -301,6 +336,20 @@ fn main() {
             }
         }
 
+        // PLAYER CONTROL
+        if input.up.is_down {
+            map.tanks.get_mut(&playerid).unwrap().move_in_dir((0.,-1.));
+        }
+        if input.down.is_down {
+            map.tanks.get_mut(&playerid).unwrap().move_in_dir((0.,1.));
+        }
+        if input.left.is_down {
+            map.tanks.get_mut(&playerid).unwrap().move_in_dir((-1.,0.));
+        }
+        if input.right.is_down {
+            map.tanks.get_mut(&playerid).unwrap().move_in_dir((1.,0.));
+        }
+
         // PHYSICS
 
         // at the end of physics, update all physics
@@ -316,7 +365,7 @@ fn main() {
 
         // Render all tanks
         for tank in &map.tanks {
-            tank.1.render(&mut canvas, &mut camera);
+            tank.1.render(&mut canvas, &mut camera, &textures);
         }
 
         canvas.present();
